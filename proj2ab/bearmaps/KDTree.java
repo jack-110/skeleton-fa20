431382b49fd221b2841e1ada38a71bd8bde77a82
陈洋 <13990779978@163.com>
2020-11-21 21:19:53 +0800

    first version of proj2a

--- proj2ab/bearmaps/KDTree.java
+++ proj2ab/bearmaps/KDTree.java
@@ -1,45 +1,50 @@
 package bearmaps;
 import java.util.List;
+
 public class KDTree implements PointSet{
 
-
     private Node root;
-
     private class Node{
         private Node right;
         private Node left;
         private Point p;
-        public Node(Node r,Node l,Point point){
+        int horizontal; // 1 : horizontal; 0 : vertical.
+        public Node(Node r,Node l,Point point, int orientation){
             right = r;
             left = l;
+            horizontal = orientation;
             p = point;
         }
     }
 
     /* the size of the list of points >= 1.*/
     public KDTree(List<Point> points){
-        root = new Node(null,null,points.get(0));
-        this.add(root,points);
+        root = new Node(null,null,points.get(0),1);
+        add(root,points);
     }
     /* add points into KDTree.*/
     private void add(Node root,List<Point> points){
         int i = 1;
         while (i < points.size()){
-            compareX(root, points.get(i));
+            add(root,points.get(i));
             i++;
         }
+    }
+    /* add a point into KdTree.*/
+    private void add(Node root, Point p){
+        compareX(root, p);
     }
     /* if less then go left,else go to right.*/
     private void compareX(Node node,Point p){
         if(p.getX() < node.p.getX()){
             if(node.left == null){
-                node.left = new Node(null,null,p);
+                node.left = new Node(null,null,p,0);
             }else {
                 compareY(node.left,p);
             }
         }else {
             if (node.right == null) {
-                node.right = new Node(null, null, p);
+                node.right = new Node(null, null, p,0);
             } else {
                 compareY(node.right, p);
             }
@@ -49,13 +54,13 @@ public class KDTree implements PointSet{
     private void compareY(Node node, Point p){
         if(p.getY() < node.p.getY()){
             if(node.left == null){
-                node.left = new Node(null,null,p);
+                node.left = new Node(null,null,p,1);
             }else {
                 compareX(node.left,p);
             }
         }else {
             if (node.right == null) {
-                node.right = new Node(null, null, p);
+                node.right = new Node(null, null, p,1);
             } else {
                 compareX(node.right, p);
             }
@@ -63,19 +68,56 @@ public class KDTree implements PointSet{
     }
     @Override
     public Point nearest(double x, double y) {
-        Node best = nearest(this.root,new Point(x,y),this.root);
-        return best.p;
+        Node nearnode = nearest(this.root,new Point(x,y),this.root);
+        return nearnode.p;
     }
 
     /* return a point whose distance from goal is less than best. */
     private Node nearest(Node n, Point goal, Node best){
+        Node good,bad;
         if(n == null){
             return best;
         }else if (Point.distance(n.p,goal) < Point.distance(goal,best.p)){
             best = n;
         }
-        best = nearest(n.left,goal,best);
-        best = nearest(n.right,goal,best);
+        if(goodSide(n,goal) == n.left){
+            good = n.left;
+            bad = n.right;
+        }else {
+            good = n.right;
+            bad = n.left;
+        }
+        best = nearest(good,goal,best);
+        if(mayBetterPoint(bad,goal,best)){
+            best = nearest(bad,goal,best);
+        }
         return best;
+    }
+
+    /* tell which side is true or not.*/
+    private Node goodSide(Node n,Point goal){
+        if(n.right == null || n.left == null){
+            return null;
+        }
+        Double leftdistance = Point.distance(n.left.p,goal);
+        Double rightdistance = Point.distance(n.right.p,goal);
+        if(leftdistance < rightdistance){
+            return n.left;
+        }else {
+            return n.right;
+        }
+    }
+    /* pruning rules:is there has possible better point in bad side.*/
+    private boolean mayBetterPoint(Node n,Point goal,Node best) {
+        if(n == null){
+            return false;
+        }
+        Point BestPoint = n.p;
+        Double BestDistance = Math.sqrt(Point.distance(goal, best.p));
+        if (n. horizontal == 1) {
+            return Math.abs(n.p.getX() - goal.getX()) < BestDistance;
+
+        }
+        return Math.abs(n.p.getY() - goal.getY()) < BestDistance;
     }
 }
--- proj2ab/bearmaps/KDTreeTest.java
+++ proj2ab/bearmaps/KDTreeTest.java
@@ -2,7 +2,9 @@ package bearmaps;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -11,37 +13,44 @@ public class KDTreeTest {
 
     @Test
     public void nearestTest() {
-
-        Point p1 = new Point(2, 3.1); // constructs a Point with x = 1.1, y = 2.2
-        Point p2 = new Point(3.3, 4.4);
-        Point p3 = new Point(-2.9, 4.2);
-        Point p4 = new Point(2, 3);
-        Point p5 = new Point(1, 3);
-        KDTree kdt = new KDTree(List.of(p1, p2, p3, p4, p5));
+        Point p1 = new Point(2, 3); // constructs a Point with x = 1.1, y = 2.2
+        Point p2 = new Point(1, 5);
+        Point p3 = new Point(4, 2);
+        Point p4 = new Point(4, 5);
+        Point p5 = new Point(3, 3);
+        Point p6 = new Point(4, 4); // constructs a Point with x = 1.1, y = 2.2
+        KDTree kdt = new KDTree(List.of(p1, p2,p3,p4,p5,p6));
         Point actual = kdt.nearest(0, 0);
-        assertEquals(p5, actual);
+        assertEquals(p1, actual);
+    }
+
+    public static List<Point> generatePoints(){
+        List<Point> lp = new ArrayList<Point>();
+        Random random = new Random(1000);
+        for(int i = 0; i < 999; i++){
+            Double x = (random.nextDouble() * 2 - 1) * 1000;
+            Double y = (random.nextDouble() * 2 - 1) * 1000;
+            lp.add(new Point(x,y));
+        }
+        return lp;
     }
 
     public static void main(String[] args) {
-        Point p1 = new Point(2, 3.1); // constructs a Point with x = 1.1, y = 2.2
-        Point p2 = new Point(3.3, 4.4);
-        Point p3 = new Point(-2.9, 4.2);
-        Point p4 = new Point(2, 3);
-        Point p5 = new Point(1, 3);
-        KDTree kdt = new KDTree(List.of(p1, p2, p3, p4, p5));
+        List<Point> points = generatePoints();
+        KDTree kdt = new KDTree(points);
         long start1 = System.currentTimeMillis();
-        for(int i = 0; i < 100000; i++){
+        for(int i = 0; i < 1000000; i++){
             kdt.nearest(0,0);
         }
         long end1 = System.currentTimeMillis();
         System.out.println( end1 - start1);
 
-        NaivePointSet naivePointSet = new NaivePointSet(List.of(p1, p2, p3, p4, p5));
+        NaivePointSet naivePointSet = new NaivePointSet(points);
         long start2 = System.currentTimeMillis();
-        for(int i = 0; i < 100000; i++){
+        for(int i = 0; i < 1000000; i++){
             naivePointSet.nearest(0,0);
         }
         long end2 = System.currentTimeMillis();
         System.out.println( end2 - start2);
-    }
+  }
 }
