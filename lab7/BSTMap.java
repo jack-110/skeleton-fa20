ca3ac852d7d5fd01cf3e4dffa9da25edebdaca3d
unknown <>
2021-02-16 15:00:54 +0800

    BSTMap done

--- lab7/BSTMap.java
+++ lab7/BSTMap.java
@@ -2,7 +2,6 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class BSTMap<k extends Comparable<k>, v> implements Map61B<k, v> {
-
     private BST bMap;
     int size;
 
@@ -11,6 +10,7 @@ public class BSTMap<k extends Comparable<k>, v> implements Map61B<k, v> {
         private v value;
         private BST right;
         private BST left;
+
         public BST(k key, v value, BST right, BST left){
             this.key = key;
             this.value = value;
@@ -21,17 +21,47 @@ public class BSTMap<k extends Comparable<k>, v> implements Map61B<k, v> {
 
     @Override
     public void clear() {
-
+        bMap = null;
+        size = 0;
     }
 
     @Override
     public boolean containsKey(k key) {
-        return false;
+        return containskey(key,bMap);
+    }
+
+    private boolean containskey(k key,BST bst){
+        if (bst == null){
+            return false;
+        }
+        if (bst.key == key){
+            return true;
+        }
+
+        int index = bst.key.compareTo(key);
+        if (index < 0){
+            return containskey(key,bst.right);
+        }
+
+        return containskey(key,bst.left);
     }
 
     @Override
     public v get(k key) {
-        return null;
+        return get(key, bMap);
+    }
+
+    private v get(k key, BST bst){
+        if(key == bst.key){
+            return bst.value;
+        }
+
+        int index = bst.key.compareTo(key);
+        if(index < 0){
+            return get(key,bst.right);
+        }
+
+        return get(key,bst.left);
     }
 
     @Override
@@ -41,11 +71,23 @@ public class BSTMap<k extends Comparable<k>, v> implements Map61B<k, v> {
 
     @Override
     public void put(k key, v value) {
-        
+        bMap = put(key, value,bMap);
+        size++;
     }
 
-    public void printOrder(){
+    private BST put(k key, v value, BST bst){
+        if (bst == null){
+            return new BST(key, value, null, null);
+        }
 
+        int index = bst.key.compareTo(key);
+        if (index < 0){
+            bst.right = put(key, value, bst.right);
+            return bst;
+        }
+
+        bst.left = put(key, value, bst.left);
+        return bst;
     }
 
     @Override
